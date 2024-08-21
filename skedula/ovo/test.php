3b40d656a0e37645af8d26ee7ccc5d9ef8989472
artur-r <71983662+artur-r@users.noreply.github.com>
2024-08-21 19:19:19 -0300

    Retirada pasta skedula

--- skedula/ovo/test.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$diaAtualSemana = "sabado";
-$diaMes = "20";
-
-echo "<button type='button' class='btn w-25 btn-outline-primary disabled'><a href='login.php'>$diaAtualSemana  <hr>  DIA $diaMes</a></button>";
